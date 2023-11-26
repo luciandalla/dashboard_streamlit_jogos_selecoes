d8a5cf15185662c67c398d858ec02671baeba020
Lucian Maciel <luciandalla.dev@gmail.com>
2023-11-26 13:44:02 -0300

    Correção de configurações no Streamlit

--- Dashboard.py
+++ Dashboard.py
@@ -14,9 +14,8 @@ teams = teams.tolist()
 
 
 #Streamlit
-st.set_option('deprecation.showfileUploaderEncoding', False)
-st.set_page_config(layout="wide", initial_sidebar_state="expanded", theme="light")
-st.title('Análise Histórica dos confrontos entre seleções')
+st.set_page_config(layout="wide")
+st.title('Análise histórica dos confrontos entre seleções')
 
 st.write("Explore um tesouro de informações sobre os confrontos históricos entre seleções em um único lugar! \
         Nosso dataframe abrange uma extensa coleção de resultados de todos os jogos já disputados entre times nacionais, \
